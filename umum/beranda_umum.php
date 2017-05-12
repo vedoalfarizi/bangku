f7397876fee799614f72d1194c8eb64fe02300be
vedoalfarizi <vedoalfarizi@gmail.com>
2017-05-12 09:03:06 +0700

    proses penambahan buku

--- umum/beranda_umum.php
+++ umum/beranda_umum.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <html>
 <head>
     <title>Sumbang Buku</title>
@@ -5,15 +8,22 @@
 <body>
     <div id="menu">
         <div class="kotak">
+            <div class="alert"><?php if($_SESSION['pesan']!=""){
+                    echo $_SESSION['pesan'];
+                }?></div>
             <form action="../fungsi/proses.php" method="post">
                 <label for="tambah">Tambah Buku</label>
                 <input type="text" name="jbuku"><br>
                 <label for="kategori">Katagori</label>
-                <input type="checkbox" name="kat" value="fiksi">Fiksi
-                <input type="checkbox" name="kat" value="nonf">Non Fiksi<br>
+                <input type="checkbox" name="kat[]" value="fiksi">Fiksi
+                <input type="checkbox" name="kat[]" value="nonf">Non Fiksi<br>
                 <input type="submit" value="Sumbang" name="sumbang">
             </form>
         </div>
     </div>
 </body>
 </html>
+
+<?php
+    $_SESSION['pesan']="";
+?>
