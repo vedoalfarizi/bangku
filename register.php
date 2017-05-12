10d67bc18478d776a4ad20f30803408c9bf6a1a0
Dara <dartikaaniemarian@gmail.com>
2017-05-12 09:32:56 +0700

    register dan penambahan style dari dara

---
+++ register.php
@@ -0,0 +1,34 @@
+<HTML>
+<head>
+    <title>REGISTER</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+
+
+        <div class="model-form register">
+            <h1>REGISTER</h1>
+            <form>
+                <div class="kotak">
+                <div class="nomor"><span>1</span>Username
+                <input type="text" name="username"></div>
+                <div class="nomor"><span>2</span>Email
+                <input type="email" name="email" /></div>
+                <div class="nomor"><span>3</span>Password
+                <input type="text" name="nohp" /></div>
+                <div class="nomor"><span>4</span>Alamat
+                <textarea name="alamat"></textarea></div>
+                  <div class="nomor"><span>5</span>Daftar Sebagai
+
+                <select name="jenis_user" >
+                    <option value="org" >organisasi/komunitas</option>
+                    <option value="Umum" >Umum</option>
+                    </select></div>
+               </div>
+
+                <input type="submit" name="register" value="Register"/>
+
+            </form></div>
+
+</body>
+</HTML>
