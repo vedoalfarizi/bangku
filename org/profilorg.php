6c108973cfc2ce40c09e61c3cbc25e5654754e3a
Dara <dartikaaniemarian@gmail.com>
2017-05-12 12:44:51 +0700

    profil organisasi satu

---
+++ org/profilorg.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+    <title>Profil</title>
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
+</head>
+<body>
+
+<!-- ------------------------------------------ profil -------------------------------------------------------------------------- -->
+           <div>
+            <div class="model-form ">
+                <h1 class="model-form profil" >TANAH OMBAK</h1>
+                <div align="center">
+                    <img src="../img/profil.jpg" class="imgprofil">
+                </div>
+
+                <label>Deskripsi</label>
+                <p align="justify">Ruang Baca dan Kreativitas Tanah Ombak merupakan komunitas yang mewadahi
+                    anak – anak dan remaja Purus, terutama Purus III, Gang IV dan sekitarnya dengan Pendidikan
+                    alternative-kreatif. RBK Tanah Ombak merupakan sebuah litersi wadah pembelajaran, yang spiritnya
+                    membangun kecerdasan dan karakter anak – anak didiknya dengan empat hal : belajar, membaca,
+                    meningkatkan potensi bakat dan minat serta membangun kemandirian.</p>
+
+                <span>
+                <img src="../img/profil.jpg" class="img img-bulat">
+                 dartikaaniemarian@gmail.com
+                </span>
+
+                <span>
+                <img src="../img/hp.png" class="img-petak">
+                0898-2628-920
+                </span>
+
+                <span> <img src="../img/hp.png" class="img-petak">
+                 Purus III
+                </span>
+            </div>
+
+            <div class="model-form kiri">
+                 <h1 class="model-form profil" >Ubah Profil</h1>
+                 <form>
+                  <div class="nomor"><span>1</span>Ubah Biodata </div>
+                  <div class="kotak">
+
+                    <label>Nama Organisasi <input type="text"> </label>
+                    <label>Deskripsi <textarea name="Deskripsi"></textarea></label>
+                    <label>Email  <input type="email" name="email" /></label>
+                    <label> No Hp <input type="text" name="nohp" /></label>
+                    <label>Alamat <textarea name="alamat"></textarea></label>
+                    <label>Tes
+                        <select>da
+                            <option>da</option>
+                            <option>da</option>
+                            <option>da</option></select></label>
+                      <input type="submit" name="Sign Up" />
+            </div>
+
+                <div class="nomor"><span>2</span>Ubah Passwords</div>
+                <div class="kotak">
+                    <label>Password Lama <input type="password" name="pass1" /></label>
+                    <label>Password Baru<input type="password" name="pass2" /></label>
+                    <label>Confirm Password <input type="password" name="passkonfirmasi" /></label>
+                </div>
+
+                <input type="submit" name="Sign Up" />
+            </form>
+            </div>
+           </div>
+
+</body>
+</html>
