324bf512f1097f69510008988c9ef29a62753189
vedoalfarizi <vedoalfarizi@gmail.com>
2017-05-12 07:46:34 +0700

    koneksi dan index

---
+++ fungsi/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db   = 'bangku';
+
+    $kon = new mysqli($host, $user, $pass, $db);
+    if($kon){
+        echo "Berhasil";
+    }
+?>
