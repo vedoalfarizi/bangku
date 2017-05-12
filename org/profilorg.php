e25bc98c3490cf2b6978f83225fe525d638dd607
Dara <dartikaaniemarian@gmail.com>
2017-05-12 14:21:19 +0700

    perbaiki profil

--- org/profilorg.php
+++ org/profilorg.php
@@ -6,38 +6,51 @@
 <body>
 
 <!-- ------------------------------------------ profil -------------------------------------------------------------------------- -->
-           <div>
-            <div class="model-form ">
-                <h1 class="model-form profil" >TANAH OMBAK</h1>
-                <div align="center">
-                    <img src="../img/profil.jpg" class="imgprofil">
-                </div>
+           <v>
+               <div>
+                   <div class="model-form ">
+                       <h1 class="model-form profil" >TANAH OMBAK</h1>
+                       <div align="center">
+                           <img src="../img/Pallet.png" class="imgprofil">
+                       </div>
 
-                <label>Deskripsi</label>
-                <p align="justify">Ruang Baca dan Kreativitas Tanah Ombak merupakan komunitas yang mewadahi
-                    anak – anak dan remaja Purus, terutama Purus III, Gang IV dan sekitarnya dengan Pendidikan
-                    alternative-kreatif. RBK Tanah Ombak merupakan sebuah litersi wadah pembelajaran, yang spiritnya
-                    membangun kecerdasan dan karakter anak – anak didiknya dengan empat hal : belajar, membaca,
-                    meningkatkan potensi bakat dan minat serta membangun kemandirian.</p>
+                       <label>Deskripsi</label>
+                       <p align="justify">Ruang Baca dan Kreativitas Tanah Ombak merupakan komunitas yang mewadahi
+                           anak – anak dan remaja Purus, terutama Purus III, Gang IV dan sekitarnya dengan Pendidikan
+                           alternative-kreatif. RBK Tanah Ombak merupakan sebuah litersi wadah pembelajaran, yang spiritnya
+                           membangun kecerdasan dan karakter anak – anak didiknya dengan empat hal : belajar, membaca,
+                           meningkatkan potensi bakat dan minat serta membangun kemandirian.</p>
 
-                <span>
-                <img src="../img/profil.jpg" class="img img-bulat">
-                 dartikaaniemarian@gmail.com
-                </span>
+                       <span>
+                            <img src="../img/profil.jpg" class="img img-bulat">
+                             dartikaaniemarian@gmail.com
+                       </span>
 
-                <span>
-                <img src="../img/hp.png" class="img-petak">
-                0898-2628-920
-                </span>
+                       <span>
+                            <img src="../img/hp.png" class="img-petak">
+                             0898-2628-920
+                       </span>
 
-                <span> <img src="../img/hp.png" class="img-petak">
-                 Purus III
-                </span>
-            </div>
+                       <span> <img src="../img/hp.png" class="img-petak">
+                           Purus III
+                        </span>
+                       <div class="kotak">
 
-            <div class="model-form kiri">
-                 <h1 class="model-form profil" >Ubah Profil</h1>
-                 <form>
+                       <h2>Verifikasi Data</h2>
+                           <label>Untuk menverifikasi data silahkan upload ktp mu</label>
+                        <button type="file" name="ktp" class="bt bt-birusoft"/>Upload KTP </label>
+                        </div>
+                   </div>
+
+
+
+
+            <div class="model-form">
+                  <h1 class="model-form profil">Profil</h1>
+               alert
+
+                <form>
+
                   <div class="nomor"><span>1</span>Ubah Biodata </div>
                   <div class="kotak">
 
@@ -46,25 +59,31 @@
                     <label>Email  <input type="email" name="email" /></label>
                     <label> No Hp <input type="text" name="nohp" /></label>
                     <label>Alamat <textarea name="alamat"></textarea></label>
-                    <label>Tes
+                    <label>Provinsi
                         <select>da
                             <option>da</option>
                             <option>da</option>
                             <option>da</option></select></label>
-                      <input type="submit" name="Sign Up" />
-            </div>
-
+                      <label>Kota
+                          <select>da
+                              <option>da</option>
+                              <option>da</option>
+                              <option>da</option></select></label>
+                      <input type="submit" name="updatebio" value="Perbarui" />
+                  </div>
+                 </form>
                 <div class="nomor"><span>2</span>Ubah Passwords</div>
-                <div class="kotak">
-                    <label>Password Lama <input type="password" name="pass1" /></label>
-                    <label>Password Baru<input type="password" name="pass2" /></label>
-                    <label>Confirm Password <input type="password" name="passkonfirmasi" /></label>
-                </div>
+               <div class="kotak">
+                   <label>Password Lama <input type="password" name="pass1" /></label>
+                   <label>Password Baru<input type="password" name="pass2" /></label>
+                   <label>Confirm Password <input type="password" name="passkonfirmasi" /></label>
+               </div>
 
-                <input type="submit" name="Sign Up" />
-            </form>
-            </div>
+               <input type="submit" name="ganti" value="Ganti Password" />
+
+                   </div>
            </div>
+
 
 </body>
 </html>
