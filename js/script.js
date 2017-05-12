b5fbc52af87f6612a7e360cff19d706d940716d5
ibrahimPntr <mahfuzjailaniibrahim@gmail.com>
2017-05-12 12:35:53 +0700

    perbaiki js menu pada stylnya

--- js/script.js
+++ js/script.js
@@ -14,50 +14,55 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
-window.onclick = function() {
+window.onclick = function(modal) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
 
-//untuk tab
-var i = 10;
-
 function satu() {
-    var a = document.getElementById('a1');
-    a.style.zIndex = i;
-    // a.style.backgroundColor= 'red';
-    i++;
-    console.log("berhasil1");
-    b1 = document.getElementById('b1');
-    b1.style.backgroundColor= 'red';
-    b12 = document.getElementById('b2');
-    b12.style.backgroundColor= '';
-    b13 = document.getElementById('b3');
-    b13.style.backgroundColor= '';
+    var a1 = document.getElementById('a1');
+    var b1 = document.getElementById('a2');
+    var c1 = document.getElementById('a3');
+
+    a1.style.display = 'block';
+    b1.style.display = 'none';
+    c1.style.display = 'none';
+    // console.log("berhasil1");
+    // b1 = document.getElementById('b1');
+    // b1.style.backgroundColor= 'red';
+    // b12 = document.getElementById('b2');
+    // b12.style.backgroundColor= '';
+    // b13 = document.getElementById('b3');
+    // b13.style.backgroundColor= '';
+}
+window.onload() = function () {
+    var qq = document.getElementById('a1');
+    var qw = document.getElementById('a2');
+    var qe = document.getElementById('a3');
+
+    qq.style.display = 'block';
+    qw.style.display = 'none';
+    qe.style.display = 'none';
 }
 function dua() {
-    var b = document.getElementById('a2');
-    b.style.zIndex = i;
-    i++;
-    console.log("berhasil2");
-    var b2 = document.getElementById('b2');
-    b2.style.backgroundColor= 'blue';
-    b22 = document.getElementById('b1');
-    b22.style.backgroundColor= '';
-    b23 = document.getElementById('b3');
-    b23.style.backgroundColor= '';
+    var a2 = document.getElementById('a1');
+    var b2 = document.getElementById('a2');
+    var c2 = document.getElementById('a3');
+
+    a2.style.display = 'none';
+    b2.style.display = 'block';
+    c2.style.display = 'none';
+
 }
 function tiga() {
-    var c = document.getElementById('a3');
-    c.style.zIndex = i;
-    i++;
-    console.log("berhasil3");
-    var b3 = document.getElementById('b3');
-    b3.style.backgroundColor= 'green';
-    b32 = document.getElementById('b1');
-    b32.style.backgroundColor= '';
-    b33 = document.getElementById('b2');
-    b33.style.backgroundColor= '';
+    var a3 = document.getElementById('a1');
+    var b3 = document.getElementById('a2');
+    var c3 = document.getElementById('a3');
+
+    a3.style.display = 'none';
+    b3.style.display = 'none';
+    c3.style.display = 'block';
 }
+
 //------------------------------------------------------
