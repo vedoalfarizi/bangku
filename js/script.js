00575f57ad89f22594f5952acc0d413e3975d947
ibrahimPntr <mahfuzjailaniibrahim@gmail.com>
2017-05-12 09:33:20 +0700

    isi js dengan js modal

--- js/script.js
+++ js/script.js
@@ -2,4 +2,20 @@
  * Created by MJI on 12/05/2017.
  */
 
+var modal = document.getElementById('modal1');
+var btn = document.getElementById("ambilbtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
 
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function() {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
